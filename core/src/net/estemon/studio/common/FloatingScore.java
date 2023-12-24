0bd9d9cb2978ce786b980d12d12706226262e58e
aaronFortuno <aaron.fortuno@protonmail.com>
2023-12-24 09:22:45 +0100

    Adding FloatingScore effect

--- core/src/net/estemon/studio/common/FloatingScore.java
+++ core/src/net/estemon/studio/common/FloatingScore.java
@@ -1,6 +1,7 @@
 package net.estemon.studio.common;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Pool;
 
 import net.estemon.studio.config.GameConfig;
@@ -8,11 +9,13 @@ import net.estemon.studio.config.GameConfig;
 public class FloatingScore implements Pool.Poolable {
 
     // attributes
-    private Color color = Color.CORAL;
+    private Color color = Color.CORAL.cpy();
 
     private int score;
     private float startX;
     private float startY;
+    private float x;
+    private float y;
     private float timer;
 
 
@@ -20,6 +23,22 @@ public class FloatingScore implements Pool.Poolable {
     public FloatingScore() {}
 
     // public methods
+    public void update(float delta) {
+        if (isFinished()) {
+            return;
+        }
+
+        timer += delta;
+
+        float percentage = timer / GameConfig.FLOATING_DURATION;
+        float alpha = MathUtils.clamp(1f - percentage, 0f, 1f);
+
+        x = startX;
+        y = startY + percentage * 60;
+
+        color.a = alpha;
+    }
+
     public void setStartPosition(float startX, float startY) {
         this.startX = startX;
         this.startY = startY;
@@ -34,24 +53,17 @@ public class FloatingScore implements Pool.Poolable {
     }
 
     public float getX() {
-        return startX;
+        return x;
     }
 
     public float getY() {
-        return startY;
+        return y;
     }
 
     public String getScoreString() {
         return Integer.toString(score);
     }
 
-    public void update(float delta) {
-        if (isFinished()) {
-            return;
-        }
-
-        timer += delta;
-    }
 
     public boolean isFinished() {
         return timer >= GameConfig.FLOATING_DURATION;
@@ -63,5 +75,8 @@ public class FloatingScore implements Pool.Poolable {
         startX = 0f;
         startY = 0f;
         timer = 0f;
+        x = 0f;
+        y = 0f;
+        color.a = 1f;
     }
 }
